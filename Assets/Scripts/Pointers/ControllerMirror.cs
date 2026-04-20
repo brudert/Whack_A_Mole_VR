be444cc4cd4d5b4be87e62c66b49b04c9f991511
brudert <tom.bruder@proton.me>
2026-04-20 13:29:47 +0200

    added mirror model while geometric mirror is on
    Added a mirror to the hand model that mirrors the controller when
    geometric mirror is active

--- Assets/Scripts/Pointers/ControllerMirror.cs
+++ Assets/Scripts/Pointers/ControllerMirror.cs
@@ -76,13 +76,9 @@ public class ControllerMirror : MonoBehaviour
         newPos = controllerToMirror.transform.position;
         newPos = new Vector3(newPos.x * -1f, newPos.y, newPos.z);
 
-        // mirror the y and z rotational axes, and keep the x axis intact.
-        // We do this by inversing X before we pass it to Quaternion.Inverse()
-        newRot = Quaternion.Euler(new Vector3(-controllerToMirror.transform.eulerAngles.x, controllerToMirror.transform.eulerAngles.y, controllerToMirror.transform.eulerAngles.z));
-        // Multiply the rotation by the base calibration angles before inversion.
-        // This ensures we get the right controller orientation.
-        newRot = Quaternion.Inverse(newRot * Quaternion.Euler(this.transform.root.eulerAngles));
-
+        // mirror the x and z rotational axes, and keep the y axis intact.
+        newRot = Quaternion.Euler(new Vector3(controllerToMirror.transform.eulerAngles.x, -controllerToMirror.transform.eulerAngles.y, controllerToMirror.transform.eulerAngles.z));
+        
         this.transform.position = newPos;
         this.transform.localRotation = newRot;
 
